ad0f6e923027b21640bbb587ef35dfc2f6efc217
jummy <929440925@qq.com>
2019-10-12 10:18:39 +0800

    add 修改分类api

--- blog-api/controllers/category.go
+++ blog-api/controllers/category.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"go-blog-api/blog-api/models"
+	"go-blog-api/blog-api/util"
 
 	"time"
 
@@ -37,7 +38,53 @@ func (c *NewCategory) Post() {
 		category.UserId = uid
 		category.Time = time.Now()
 		logs.Info(o.Insert(category))
-		c.Data["json"] = models.Message{Code: 200, Result: "添加分类成功", Data: category}
+		var user models.User
+		_, err = o.QueryTable("user").Filter("Uid", uid).All(user)
+		if err != nil {
+			logs.Info(nil)
+		}
+		userView := util.UserToViews(user)
+		categoryView := util.CategoryToView(*category, *userView)
+
+		c.Data["json"] = models.Message{Code: 200, Result: "添加分类成功", Data: categoryView}
+		c.ServeJSON()
+	}
+}
+
+//UpdateCategory 修改文章
+type UpdateCategory struct {
+	beego.Controller
+}
+
+//Put /api/category/update_category
+func (c *UpdateCategory) Put() {
+	token := c.Ctx.Request.Header.Get("token")
+	_, isAdmin := models.IsAdmin(token)
+	if isAdmin == false {
+		c.Data["json"] = models.Message{Code: 301, Result: "你不是超级管理员", Data: nil}
+		c.ServeJSON()
+	} else {
+		categoryId := c.GetString("categoryId")
+		category := models.Category{Uid: categoryId}
+		o := orm.NewOrm()
+		err := o.Read(&category, "Uid")
+		if err == orm.ErrNoRows {
+			c.Data["json"] = models.Message{Code: 200, Result: "没有这分类", Data: &category}
+			c.ServeJSON()
+		}
+		name := c.GetString("name")
+		category.Name = name
+		logs.Info(o.Update(&category))
+
+		var user models.User
+		_, err = o.QueryTable("user").Filter("uid", category.UserId).All(&user)
+		if err != nil {
+			logs.Info(err)
+		}
+		userView := util.UserToViews(user)
+		categoryView := util.CategoryToView(category, *userView)
+
+		c.Data["json"] = models.Message{Code: 200, Result: "修改成功", Data: &categoryView}
 		c.ServeJSON()
 	}
 }
